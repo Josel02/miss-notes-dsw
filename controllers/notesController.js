4d1103834039b3390b67813238dd33c72865bb13
Aitor <aitormillantrujillo@gmail.com>
2024-03-26 20:39:16 +0100

    Fix typo and update variable names in notesController.js

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -2,8 +2,8 @@ const Note = require('../models/note');
 
 exports.createNote = async (req, res) => {
   try {
-    const newNote = new Nota({
-      ...req.body, // Asume que el cuerpo de la solicitud ya tiene todos los campos necesarios
+    const newNote = new Note({
+      ...req.body,
     });
     const savedNote = await newNote.save();
     res.status(201).json(savedNote);
@@ -14,7 +14,7 @@ exports.createNote = async (req, res) => {
 
 exports.getAllNotes = async (req, res) => {
   try {
-    const notes = await Nota.find();
+    const notes = await Note.find();
     res.status(200).json(notes);
   } catch (error) {
     res.status(500).json({ message: 'Error getting the notes: ' + error.message });
@@ -24,21 +24,21 @@ exports.getAllNotes = async (req, res) => {
 exports.updateNote = async (req, res) => {
   const { id } = req.params;
   try {
-    const updatedNote = await Nota.findByIdAndUpdate(id, req.body, { new: true });
+    const updatedNote = await Note.findByIdAndUpdate(id, req.body, { new: true });
     res.status(200).json(updatedNote);
   } catch (error) {
     res.status(400).json({ message: 'Error updating the note: ' + error.message });
   }
 };
 
-exports.findNoteById = async (req, res) => {
+exports.getNoteById = async (req, res) => {
   const { id } = req.params;
   try {
     const note = await Note.findById(id);
     if (note) {
       res.json(note);
     } else {
-      res.status(404).json({ message: 'Nota no encontrada' });
+      res.status(404).json({ message: 'Note no encontrada' });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -48,7 +48,7 @@ exports.findNoteById = async (req, res) => {
 exports.deleteNote = async (req, res) => {
   const { id } = req.params;
   try {
-    await Nota.findByIdAndDelete(id);
+    await Note.findByIdAndDelete(id);
     res.status(204).send();
   } catch (error) {
     res.status(500).json({ message: 'Error deleting the note: ' + error.message });
