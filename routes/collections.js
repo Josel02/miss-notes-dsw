6853d152ada25bb3ff48269b9dd0c44fe66f733d
Josel02 <Jose.Lara3@alu.uclm.es>
2024-05-09 20:41:00 +0200

    Cambiadas de sitio rutas de colecciones y de notas

--- routes/collections.js
+++ routes/collections.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const collectionsController = require('../controllers/collectionsController');
 const verifyTokenAndRole = require('../middleware/authMiddleware');
 
+// Rutas administrativas para operaciones globales en colecciones
+router.get('/admin/collections', verifyTokenAndRole("Admin"), collectionsController.getCollectionsByAdmin); // Obtener todas las colecciones de todos los usuarios
+router.post('/admin/collections', verifyTokenAndRole("Admin"), collectionsController.createCollectionByAdmin); // Crear una nueva colección para cualquier usuario
+router.put('/admin/collections/:id', verifyTokenAndRole("Admin"), collectionsController.updateCollectionByAdmin); // Editar cualquier colección de cualquier usuario
+router.delete('/admin/collections/:id', verifyTokenAndRole("Admin"), collectionsController.deleteCollectionByAdmin); // Borrar cualquier colección de cualquier usuario
+router.put('/admin/collections/:id/notes/add', verifyTokenAndRole("Admin"), collectionsController.addNotesToCollectionByAdmin); // Añadir múltiples notas a una colección para cualquier usuario
+
 // Rutas básicas para operaciones CRUD en colecciones
 router.post('/', verifyTokenAndRole(), collectionsController.createCollection); // Crear una nueva colección
 router.get('/', verifyTokenAndRole(), collectionsController.getCollectionsByUser); // Obtener todas las colecciones de un usuario
@@ -20,12 +27,5 @@ router.patch('/unshare/:id', verifyTokenAndRole(), collectionsController.unshare
 
 // Rutas de utilidad para trabajar con notas dentro de colecciones
 router.get('/note/:noteId/collections', verifyTokenAndRole(), collectionsController.getCollectionsContainingNote); // Obtener todas las colecciones que contienen una nota específica
-
-// Rutas administrativas para operaciones globales en colecciones
-router.get('/admin/collections', verifyTokenAndRole("Admin"), collectionsController.getCollectionsByAdmin); // Obtener todas las colecciones de todos los usuarios
-router.post('/admin/collections', verifyTokenAndRole("Admin"), collectionsController.createCollectionByAdmin); // Crear una nueva colección para cualquier usuario
-router.put('/admin/collections/:id', verifyTokenAndRole("Admin"), collectionsController.updateCollectionByAdmin); // Editar cualquier colección de cualquier usuario
-router.delete('/admin/collections/:id', verifyTokenAndRole("Admin"), collectionsController.deleteCollectionByAdmin); // Borrar cualquier colección de cualquier usuario
-router.put('/admin/collections/:id/notes/add', verifyTokenAndRole("Admin"), collectionsController.addNotesToCollectionByAdmin); // Añadir múltiples notas a una colección para cualquier usuario
 
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -5,9 +5,6 @@ const verifyTokenAndRole = require('../middleware/authMiddleware');
 
 // Definir primero rutas específicas antes de rutas con parámetros
 
-//Ruta para actualizar las colecciones de una nota
-router.put('/:noteId/collections', verifyTokenAndRole(), notesController.updateNoteCollections);
-
 //Ruta para obtener todas las notas de un usuario donde es el propietario
 //router.get('/user', verifyTokenAndRole(), notesController.getNotesByUser);
 router.get('/user', verifyTokenAndRole(), notesController.getMyNotes);
@@ -17,12 +14,6 @@ router.get('/user', verifyTokenAndRole(), notesController.getMyNotes);
 
 // Crear una nueva nota para el usuario autenticado
 router.post('/', verifyTokenAndRole(), notesController.createNote);
-
-// Actualizar una nota existente que pertenece al usuario autenticado
-router.put('/:id', verifyTokenAndRole(), notesController.updateNote);
-
-// Eliminar una nota que pertenece al usuario autenticado
-router.delete('/:id', verifyTokenAndRole(), notesController.deleteNote);
 
 // Obtener todas las notas de un usuario - Solo para Admins
 router.get('/', verifyTokenAndRole("Admin"), notesController.getAllNotes);
@@ -51,5 +42,13 @@ router.post('/unshare-note', verifyTokenAndRole(), notesController.unshareNote);
 // Actualiza la lista de usuarios con los que se comparte una nota propietaria
 router.put('/update-shared-users', verifyTokenAndRole(), notesController.updateSharedUsers);
 
+// Actualizar una nota existente que pertenece al usuario autenticado
+router.put('/:id', verifyTokenAndRole(), notesController.updateNote);
+
+// Eliminar una nota que pertenece al usuario autenticado
+router.delete('/:id', verifyTokenAndRole(), notesController.deleteNote);
+
+//Ruta para actualizar las colecciones de una nota
+router.put('/:noteId/collections', verifyTokenAndRole(), notesController.updateNoteCollections);
 
 module.exports = router;
