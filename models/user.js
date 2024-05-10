c557621883e091a84f4f0b436956b6cf2050bf09
Aitor <aitormillantrujillo@gmail.com>
2024-05-10 07:04:12 +0200

    chore: Update user model to delete friendships when deleting a user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Note = require('./note');
 const Collection = require('./collection');
+const Friendship = require('./friendship');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -9,12 +10,19 @@ const userSchema = new mongoose.Schema({
   role: { type: String, enum: ['Admin', 'User'], default: 'User' }
 });
 
-// Eliminar todas las notas y colecciones asociadas a un usuario antes de eliminarlo
+// Eliminar todas las notas, colecciones y friendships asociadas a un usuario antes de eliminarlo
 userSchema.pre('findOneAndDelete', async function(next) {
   const docToDelete = await this.model.findOne(this.getFilter());
   if (docToDelete) {
     await Note.deleteMany({ userId: docToDelete._id });
     await Collection.deleteMany({ userId: docToDelete._id });
+    // Eliminar todas las friendships donde el usuario es requester o receiver
+    await Friendship.deleteMany({
+      $or: [
+        { requester: docToDelete._id },
+        { receiver: docToDelete._id }
+      ]
+    });
   }
   next();
 });
